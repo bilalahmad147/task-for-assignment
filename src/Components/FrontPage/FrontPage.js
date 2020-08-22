9cb85e904681636661fd5c4e647826785baf553b
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-08-21 21:48:52 -0800

    Update FrontPage.js

--- src/Components/FrontPage/FrontPage.js
+++ src/Components/FrontPage/FrontPage.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import style from './Front.module.css';
-import useWebAnimations, {rotateInDownLeft} from "@wellyshen/use-web-animations";
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function FrontPage() {
-    const { ref } = useWebAnimations({...rotateInDownLeft});
+    const { ref } = useWebAnimations({
+        keyframes: [
+            { transform: 'perspective(400px) rotate3d(1, 0, 0, 90deg)', opacity: '0', offset: 0 },
+            { transform: 'perspective(400px) rotate3d(1, 0, 0, -20deg)', offset: 0.4 },
+            { transform: 'perspective(400px) rotate3d(1, 0, 0, 10deg)', opacity: '1', offset: 0.6 },
+            { transform: 'perspective(400px) rotate3d(1, 0, 0, -5deg)', opacity: '1', offset: 0.8 },
+            { transform: 'perspective(400px)', opacity: '1', offset: 1 }
+        ],
+        timing: {
+            delay: 1000,
+            duration: 3000,
+            iterations: 1
+        }
+    });
 
     return (
         <div className={style.homePage}>
