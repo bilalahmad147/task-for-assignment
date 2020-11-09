111b69a1e87ca000584fe163af95f663eb19cc3e
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-10 02:25:35 +0500

    render first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import FrontPage from './Components/FrontPage/FrontPage';
+import { emailData } from './data/emails'
 
 function App() {
   return (
     <div>
-      <FrontPage />
+      <h1>list of data</h1>
+      {
+        emailData.map((ind, key) => {
+          return (
+            <div key={ind.id}>
+              <ul>
+                <li>{ind.name}</li>
+                <li>{ind.username}</li>
+                <li>{ind.website}</li>
+              </ul>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
