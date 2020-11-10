a17ea03c55297ef616d0749b84a9410d199ad289
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-10 05:46:09 +0500

    render inbox function

--- src/components/InboxData/InboxData.js
+++ src/components/InboxData/InboxData.js
@@ -10,12 +10,47 @@ function InboxData() {
                 emailData.map((ind, key) => {
                     return (
                         <div key={ind.id} className={style.mainData}>
-                            <ul>
-                                <li>{ind.name}</li>
-                            </ul>
-                            <ul>
-                                <li>{ind.subject}</li>
-                            </ul>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Subject</th>
+                                        <th>Time</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Alfreds Futterkiste</td>
+                                        <td>Maria Anders</td>
+                                        <td>Germany</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro comercial Moctezuma</td>
+                                        <td>Francisco Chang</td>
+                                        <td>Mexico</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ernst Handel</td>
+                                        <td>Roland Mendel</td>
+                                        <td>Austria</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Island Trading</td>
+                                        <td>Helen Bennett</td>
+                                        <td>UK</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Laughing Bacchus Winecellars</td>
+                                        <td>Yoshi Tannamuri</td>
+                                        <td>Canada</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Magazzini Alimentari Riuniti</td>
+                                        <td>Giovanni Rovelli</td>
+                                        <td>Italy</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     )
                 })
